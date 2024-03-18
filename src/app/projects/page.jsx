8a4939a48a8cae74a7b6b8677a4b6a64460e6883
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-03-18 02:11:44 +0100

    preloader mouted to project page

---
+++ src/app/projects/page.jsx
@@ -0,0 +1,10 @@
+export default function ProjectPage() {
+    
+    return (
+        <div className='grid__center full__size'>
+            <h1>
+                Projects
+            </h1>
+        </div>
+    )
+}
