5444fc4481a42634071eac15cd8045d029f7963e
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-03-18 02:11:55 +0100

    Create metaDataBase.js

---
+++ src/webContents/metaDataBase.js
@@ -0,0 +1,10 @@
+export const homePageMeta = {
+    title: `Akanda Dev`,
+    description: `A Creative software engineer with experience in designing and implementing user-friendly interfaces and functional software solutions for complex business problems.`,
+    themeColor: `#9800BE`,
+    img: `/images/metaPreviewImage.png`,
+    url: `https://www.akanda.dev/`,
+    type: `website`,
+    locale: `en_NG`,
+
+}
