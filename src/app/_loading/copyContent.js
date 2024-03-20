21c2af4caa845f76d40979b60633af845fcefa24
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-03-20 15:53:54 +0100

    chores: more Tips and tricks contents added

--- src/app/_loading/copyContent.js
+++ src/app/_loading/copyContent.js
@@ -4,39 +4,111 @@ export const entranceFacts = [
         fact: `The first webcam video is from the University of Cambridge. And the subject of the video is coffee pot.`,
     },
     {
+        title: `Tips and Tricks`,
+        fact: `Search engine optimization (SEO) is an important aspect of web development cost, helping to improve the visibility of your web applications in search engine results pages (SERPs).`,
+    },
+    {
         title: `Do You Know?`,
         fact: `Google average almost 3 billion searches per day.`,
+    },
+    {
+        title: `Tips and Tricks`,
+        fact: `CSS preprocessors like Sass or Less can make writing CSS more efficient and organized, allowing you to write code faster and with fewer errors.`,
+    },
+    {
+        title: `Do You Know?`,
+        fact: `Nah everything you no dey sabi ni, wetin you know gangan, oya tell me... 😏`,
+    },
+    {
+        title: `Tips and Tricks`,
+        fact: `CSS preprocessors like Sass or Less can make writing CSS more efficient and organized, allowing you to write code faster and with fewer errors.`,
     },
     {
         title: `Do You Know?`,
         fact: `Symbolics.com was the first registered domain`,
     },
     {
+        title: `Tips and Tricks`,
+        fact: `Containerization tools like Docker or Kubernetes can help you manage and deploy web applications more efficiently and reliably, by encapsulating them in self-contained units called containers.`,
+    },
+    {
         title: `Do You Know?`,
         fact: `More Than 50% Of Internet Users Make E-commerce Purchases Weekly.`,
+    },
+    {
+        title: `Tips and Tricks`,
+        fact: `Virtualization tools like Vagrant can help you create development environments that mirror your production environment, improving consistency and reliability.`,
     },
     {
         title: `Do You Know?`,
         fact: `The first website went live in 1991`,
     },
     {
+        title: `Tips and Tricks`,
+        fact: `Progressive web apps are web applications that provide a native app-like experience to users, by using modern web technologies like service workers and web app manifests.`,
+    },
+    {
         title: `Do You Know?`,
         fact: `The double slash “//” in URLs was an idea Berners-Lee copied from the Apollo workstation’s ‘domain’ file system.`,
+    },
+    {
+        title: `Tips and Tricks`,
+        fact: `Use the Spread Operator to Copy and Merge Arrays... e.g. [...Array]`,
+    },
+    {
+        title: `Do You Know?`,
+        fact: `Jennifer Lopez inspired the creation of Google images.`,
+    },
+    {
+        title: `Tips and Tricks`,
+        fact: `Use console.table() to Display Arrays and Objects in the Console`,
+    },  
+    {
+        title: `Do You Know?`,
+        fact: `Demographics play a huge role in user behavior, 9ja no be Yanki... 🤐😜🥱`,
+    },
+    {
+        title: `Tips and Tricks`,
+        fact: `Use Destructuring to Extract Properties from Objects`,
     },
     {
         title: `Do You Know?`,
         fact: `Social Media Platforms Dominate Among Internet Users.`,
     },
     {
+        title: `Tips and Tricks`,
+        fact: `Content management systems like WordPress, Drupal, or Joomla can help you build and manage complex websites more easily, by providing pre-built templates, plugins, and integrations.`,
+    },
+    {
         title: `Do You Know?`,
         fact: `The life span of a website is just 3 years`,
+    },
+    {
+        title: `Tips and Tricks`,
+        fact: `You want more tricks? kuku turn me to magician nah... 😒`,
     },
     {
         title: `Do You Know?`,
         fact: `The first websites only had words and pictures`,
     },
     {
+        title: `Tips and Tricks`,
+        fact: `Remove Duplicates from an Array with Set and Map`,
+    },
+    {
         title: `Do You Know?`,
         fact: `The Internet Hit 5.1 Billion Users in 2023.`,
-    }   
+    },
+    {
+        title: `Tips and Tricks`,
+        fact: `Tips no dey tire you? when you no be 9ja popo... 🤣🤣🤣`,
+    },  
+    {
+        title: `Do You Know?`,
+        fact: `We are more creative in the shower. Oya go bath... 😉😉😉`,
+    },
+    {
+        title: `Tips and Tricks`,
+        fact: `Say bye bye to UP NEPA!!! Cat urine glows under a black light.... 😸😻🙀`,
+    },
 ]
