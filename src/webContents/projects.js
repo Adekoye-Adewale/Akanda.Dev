930fb005987ed227b901c871a7af73a5e3d7d1c8
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-15 22:25:04 +0100

    Created project objects file

---
+++ src/webContents/projects.js
@@ -0,0 +1,74 @@
+export const ProjectFile = [
+    {
+        client:`Mediatek NG`,
+        industry:`Advertisments`,
+        summary:`A media independent company`,
+        status:`Completed`,
+        year:`2022`,
+        img: {
+            src: ``,
+            alt: ``,
+            title: ``,
+            width: ``,
+            height: ``,
+        },
+        url: {
+            href: `https://www.mediatekng.com/`,
+            title: `Click to visit Mediatek NG website`,
+        },
+        category: `Corporate Website`,
+        technologies: ['WordPress', `Elementor`, `Google Tag Manager`],
+        agency: {
+            name: `Freelence`,
+            url: `/`,
+        }
+    },
+    {
+        client:`Reset USA`,
+        industry:`Active Wears`,
+        summary:``,
+        status:`Completed`,
+        year:`2022`,
+        img: {
+            src: ``,
+            alt: ``,
+            title: ``,
+            width: ``,
+            height: ``,
+        },
+        url: {
+            href: `https://www.reset-usa.com/`,
+            title: `Click to visit Reset USA website`,
+        },
+        category: `Ecommerce Website`,
+        technologies: ['WordPress', `WooCommerces`, `Elementor`, `Google Tag Manager`],
+        agency: {
+            name: `Freelence`,
+            url: `/`,
+        }
+    },
+    {
+        client:`POF Luxury`,
+        industry:`Fashion Brand`,
+        summary:``,
+        status:`Completed`,
+        year:`2023`,
+        img: {
+            src: ``,
+            alt: ``,
+            title: ``,
+            width: ``,
+            height: ``,
+        },
+        url: {
+            href: `/`,
+            title: `Click to visit POF Luxury website`,
+        },
+        category: `Ecommerce Website`,
+        technologies: ['WordPress', `WooCommerces`, `Elementor`, `Google Tag Manager`],
+        agency: {
+            name: `Techduce Africa`,
+            url: `/`,
+        }
+    },
+]
