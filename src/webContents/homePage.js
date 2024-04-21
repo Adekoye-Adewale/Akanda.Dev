6f5581114e01ef316515ddfb7cc87ffc75970c22
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-21 18:55:29 +0100

    Create homePage.js

---
+++ src/webContents/homePage.js
@@ -0,0 +1,47 @@
+export const HomeContent = {
+    intro: `In the business of putting everyone online.`,
+    introAbout: `With a focus on the digital realm, Akanda specializes in website design and development, mobile app development, analytics, and SEO.`,
+    about: `Akanda forte is taking industry leaders forward with digital products that focus on and meet their wants and needs.`,
+    skill: [
+        {
+            title: 'Website Design',
+            description: 'We design and develop websites that are easy to use and easy to navigate.',
+            since: `2019`
+        },
+        {
+            title: 'Website Design',
+            description: 'We design and develop websites that are easy to use and easy to navigate.',
+            since: `2019`
+        },
+        {
+            title: 'Website Design',
+            description: 'We design and develop websites that are easy to use and easy to navigate.',
+            since: `2019`
+        },
+        {
+            title: 'Website Design',
+            description: 'We design and develop websites that are easy to use and easy to navigate.',
+            since: `2019`
+        },
+    ],
+    whyAkanda: `Creating websites and applications is my passion, and I am dedicated to doing everything in my power to ensure the entire process aligns with your business objectives.`,
+    whyAkandaList: [
+        {
+            title: `High Quality`,
+        },
+        {
+            title: `Quality Assurance`,
+        },
+        {
+            title: `Swift Support`,
+        },
+        {
+            title: `No Bull-Sh*t`,
+        },
+    ],
+    cta: {
+        title: `Connect With Akanda`,
+        description: `Are you ready to elevate your business to new heights? Akanda.Dev specializes in crafting stunning websites that not only captivate your audience but also drive results. As a seasoned website developer with a keen eye for detail, I'm dedicated to delivering unparalleled quality on every project.`,
+        desc: `From seamless user experiences to cutting-edge SEO strategies, our services are tailored to help your business thrive in the digital landscape. Let's collaborate to transform your online vision into reality and boost your revenue like never before.`,
+    }
+}
