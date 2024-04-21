15b7beef27bbe2ce838f8d3eeb3f534bab63d7fb
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-21 18:55:33 +0100

    Create servicePage.js

---
+++ src/webContents/servicePage.js
@@ -0,0 +1,171 @@
+export const Services = {
+    intro: `Services tailored to making your business visible on the internet`,
+    services: [
+        {
+            id: `00001`,
+            title: `Website Development`,
+            subTitle: `Need to make your business visible globally?`,
+            desc: `<p>Akanda.Dev creates lightning-fast websites on any device from anywhere in the globe with custom content management systems, making maintaining your digital presence a breeze, and on-demand custom-built web applications using the best technologies.</p>`,
+            img: {
+                src: `/images/`,
+                alt: `Website design and development in Lagos`,
+                title: `Website development`,
+                width: ``,
+                height: ``,
+            },
+            technologies: [
+                {
+                    title: `WordPress`,
+                    img: {
+                        src: `/images/`,
+                        alt: `WordPress website design and development in Nigeria`,
+                        title: `WordPress website design and development`,
+                        width: ``,
+                        height: ``,
+                    },
+                },
+                {
+                    title: `Webflow`,
+                    img: {
+                        src: `/images/`,
+                        alt: `Webflow website designer and developer in Lagos`,
+                        title: `Webflow website design and development`,
+                        width: ``,
+                        height: ``,
+                    },
+                },
+                {
+                    title: `WooCommerce`,
+                    img: {
+                        src: `/images/`,
+                        alt: `ECommerce website design and development in Nigeria`,
+                        title: `eCommerce website design and development`,
+                        width: ``,
+                        height: ``,
+                    },
+                },
+                {
+                    title: `Node JS`,
+                    img: {
+                        src: `/images/`,
+                        alt: `Node JS`,
+                        title: `Node JS`,
+                        width: ``,
+                        height: ``,
+                    },
+                },
+                {
+                    title: `React JS`,
+                    img: {
+                        src: `/images/`,
+                        alt: `React JS website developer in Lagos`,
+                        title: `React JS website developer in Lagos`,
+                        width: ``,
+                        height: ``,
+                    },
+                },
+                {
+                    title: `Next JS`,
+                    img: {
+                        src: `/images/`,
+                        alt: `Next JS website developer in Lagos`,
+                        title: `Next JS website developer in Lagos`,
+                        width: ``,
+                        height: ``,
+                    },
+                },
+            ]
+        },
+        {
+            id: `00002`,
+            title: `SEO Optimization`,
+            subTitle: `Dominate Your Local Market With Powerful SEO`,
+            desc: `<p>Akanda will unlock your website's potential and drive traffic across all search engines with search engine optimization (SEO).</p>
+            <p>Google’s algorithms are set up to reward solid, localized SEO practices. Akanda.Dev is a local SEO expert who can help you make sure your brand gets seen and your sales funnel gets a steady flow of leads. As a local SEO guru, Akanda constantly helps businesses get the local visibility online that they deserve. Using proven, white hat methods of boosting local search engine results, Akanda.Dev delivers measurable performance.</p>`,
+            img: {
+                src: `/images/`,
+                alt: `SEO expert in Lagos`,
+                title: `local SEO setup`,
+                width: ``,
+                height: ``,
+            },
+            technologies: [
+                {
+                    title: `Google Search Console`,
+                    img: {
+                        src: `/images/`,
+                        alt: `Google Search Console setup`,
+                        title: `Google Search Console setup`,
+                        width: ``,
+                        height: ``,
+                    },
+                },
+                {
+                    title: `Google Analytics`,
+                    img: {
+                        src: `/images/`,
+                        alt: `Google Analytics setup`,
+                        title: `Google Analytics setup`,
+                        width: ``,
+                        height: ``,
+                    },
+                },
+                {
+                    title: `HotJar`,
+                    img: {
+                        src: `/images/`,
+                        alt: `HotJar setup`,
+                        title: `HotJar installation`,
+                        width: ``,
+                        height: ``,
+                    },
+                },
+            ]
+        },
+        {
+            id: `00003`,
+            title: `Digital Design`,
+            subTitle: `Website Design that Converts and generates leads with ease`,
+            desc: `<p>Akanda is the master of the UI/UX design craft, committed to shaping digital experiences that not only dazzle the eyes or users but leave a lasting impression on the user’s journey. Akanda.Dev approaches the whole creative process through the lens of what will resonate emotionally with users, guiding them on a journey that eventually results in the completion of key actions.</p>`,
+            img: {
+                src: `/images/`,
+                alt: `website designer in Lagos`,
+                title: `ui ux designer`,
+                width: ``,
+                height: ``,
+            },
+            technologies: [
+                {
+                    title: `Figma`,
+                    img: {
+                        src: `/images/`,
+                        alt: `Figma`,
+                        title: `Figma`,
+                        width: ``,
+                        height: ``,
+                    },
+                },
+                {
+                    title: `Adobe XD`,
+                    img: {
+                        src: `/images/`,
+                        alt: `Adobe XD`,
+                        title: `Adobe XD`,
+                        width: ``,
+                        height: ``,
+                    },
+                },
+                {
+                    title: `Illustrator`,
+                    img: {
+                        src: `/images/`,
+                        alt: `Adobe Illustrator`,
+                        title: `Adobe Illustrator`,
+                        width: ``,
+                        height: ``,
+                    },
+                },
+            ]
+        },
+    ]
+}
