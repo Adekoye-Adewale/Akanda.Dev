32d805ea32206283d66056872eca74314b2ad461
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-04-21 18:55:27 +0100

    Create aboutPage.js

---
+++ src/webContents/aboutPage.js
@@ -0,0 +1,8 @@
+export const Akanda = {
+    subIntro: `Big Talent, Great Mind`,
+    intro: `Meet Adekoye Adewale, Website Developer and SEO Expert in Lagos, Nigeria`,
+    body: `With a passion for crafting exceptional digital experiences, Adekoye Adewale pka Akanda.Dev is your go-to partner for all things website development and design. Since 2019, He has been dedicated to helping businesses thrive in the digital space through innovative solutions and unmatched expertise.`,
+    quote: `Driven by a desire for excellence, I go above and beyond to ensure that each project I undertake exceeds expectations. From meticulous attention to detail to staying abreast of the latest industry trends, I pride myself on delivering results that truly make a difference for my clients.`,
+    secTwoTitle: `Akanda.Dev is always ready to develop your idea`,
+    secTwoBody: `For every website and application, Akanda combines a creative approach with an understanding of the industry on a business level. He implements tailor-made technology solutions in every project to fulfill each client's business goals.`,
+}
