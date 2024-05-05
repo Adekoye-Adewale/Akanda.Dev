c92f564cf202a420fcbd10ad13d5490792040189
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-05 02:18:01 +0100

    Create siteFooterCopy.js

---
+++ src/webContents/siteFooterCopy.js
@@ -0,0 +1,17 @@
+export const FootCard = [
+    {
+        pri: `View my`,
+        sec: `Projects`,
+        link: `/website-development-projects`,
+    },
+    {
+        pri: `Meet Akanda`,
+        sec: `Contact`,
+        link: `/contact-website-developer-in-lagos`,
+    },
+    {
+        pri: `Learn more`,
+        sec: `About us`,
+        link: `/about-akanda`,
+    },
+]
