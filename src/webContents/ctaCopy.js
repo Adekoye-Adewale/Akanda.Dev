b448b0d8999227026a9c1219827dcd4c411d6fb8
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-05 22:28:50 +0100

    Create ctaCopy.js

---
+++ src/webContents/ctaCopy.js
@@ -0,0 +1,8 @@
+export const FooterCTA = {
+    main: `Akanda.Dev specializes in crafting the website of your dreams that captivates your audience and drives results. `,
+    body: `From seamless user experiences to cutting-edge SEO strategies, Akanda's services are tailored to help your business thrive in the digital market. Let's collaborate to transform your online vision into reality and boost your revenue like never before.`,
+    link: {
+        href: `/`,
+        text: `connect with akanda`,
+    }
+}
