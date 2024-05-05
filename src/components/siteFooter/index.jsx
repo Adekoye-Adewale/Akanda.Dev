950a62cfd26959088382293b8913640930cff9cc
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-05 02:18:08 +0100

    Create index.jsx

---
+++ src/components/siteFooter/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CtaWrap from './ctaWrap'
+import Foot from './foot'
+
+export default function SiteFooter() {
+    return (
+        <footer>
+            <CtaWrap/>
+            <Foot/>
+        </footer>
+    )
+}
