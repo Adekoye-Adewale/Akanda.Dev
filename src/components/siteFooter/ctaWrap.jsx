9ef39e91cfcc5f833a8f39bdd404d97e50dd80a0
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-05 22:29:11 +0100

    Update ctaWrap.jsx

--- src/components/siteFooter/ctaWrap.jsx
+++ src/components/siteFooter/ctaWrap.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Body, Title } from '../text'
+import PriBtn from '../btn/priBtn'
+import { FooterCTA } from '@/webContents/ctaCopy'
+import style from './siteFooter.module.css'
 
 export default function CtaWrap() {
     return (
-        <div>
-            
+        <div className={style.cta__wrap}>
+            <div>
+                <Title title={FooterCTA.main}/>
+                <Body text={FooterCTA.body}/>
+            </div>
+            <div>
+                <PriBtn {...FooterCTA.link}/>
+            </div>
         </div>
     )
 }
