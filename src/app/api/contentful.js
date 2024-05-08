ffdb4c7f0b02e54b8c10230afd283247184569f0
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-08 22:38:50 +0100

    Update contentful.js

--- src/app/api/contentful.js
+++ src/app/api/contentful.js
@@ -1,6 +1,6 @@
-import * as Contentful from Contentful
+// import * as Contentful from Contentful
 
-export var client = Contentful.createClient({
-    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-    space: process.env.CONTENTFUL_SPACE_ID,
-})
+// export var client = Contentful.createClient({
+//     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+//     space: process.env.CONTENTFUL_SPACE_ID,
+// })
