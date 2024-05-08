813d30dde720cd5bd5b84124ea43442ea8af3907
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-08 22:29:48 +0100

    Update contentful.js

--- src/app/api/contentful.js
+++ src/app/api/contentful.js
@@ -1,11 +1,6 @@
-import React from 'react'
 import * as Contentful from Contentful
 
-var client = Contentful.createClient({
+export var client = Contentful.createClient({
     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
     space: process.env.CONTENTFUL_SPACE_ID,
 })
-
-export async function getPortfolio() {
-    const result = await client.getEntry();
-}
