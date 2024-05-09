2e1750d5357797961b8b083dcfe31b0da0d0c078
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-09 18:22:35 +0100

    Create accordionTitle.jsx

---
+++ src/components/accordion/accordionTitle.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SubTitle } from '../text'
+
+export default function AccordionTitle({ text }) {
+    return (
+        <>
+            <SubTitle subTitle={text}/>
+        </>
+    )
+}
