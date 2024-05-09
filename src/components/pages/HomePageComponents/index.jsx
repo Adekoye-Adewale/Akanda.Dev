a4dd06fda27671b75eb4ac6a249219e83f4b690b
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-09 21:10:50 +0100

    Update index.jsx

--- src/components/pages/HomePageComponents/index.jsx
+++ src/components/pages/HomePageComponents/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { SubTitle, Title } from '@/components/text'
+import { Body, SubTitle, Title } from '@/components/text'
 import Tab from '@/components/tab'
 import { HomeContent } from '@/webContents/homePage'
 import style from './homePage.module.css'
 import Accordion from '@/components/accordion'
+import { FAQ } from '@/webContents/faq'
+import SecBtn from '@/components/btn/secBtn'
 
 export default function HomePage() {
     return (
@@ -17,8 +19,15 @@ export default function HomePage() {
                 </div>
                 <Tab/>
             </section>
-            <section>
-                <Accordion/>
+            <section className={style.faq__sec}>
+                <div>
+                    <Title title={HomeContent.faq.title}/>
+                </div>
+                <div>
+                    <Body text={HomeContent.faq.desc}/>
+                    <SecBtn {...HomeContent.faq.btn}/>
+                </div>
+                <Accordion Content={FAQ}/>
             </section>
         </>
     )
