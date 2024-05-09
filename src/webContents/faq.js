42798c16103995c53c401d21a683193e955ed241
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-09 21:10:31 +0100

    Update faq.js

--- src/webContents/faq.js
+++ src/webContents/faq.js
@@ -1,23 +1,28 @@
 export const FAQ = [
     {
-        Q: `What services does Akanda offer?`,
-        A: `Akanda offers a variety of services, but his main areas of focus are web design and development, mobile applications, digital transformation consulting services, and SEO. Some other services include content marketing, branding, and graphic design.`,
+        title: 'q1',
+        question: `What services does Akanda offer?`,
+        answer: `Akanda offers a variety of services, but his main areas of focus are web design and development, mobile applications, digital transformation consulting services, and SEO. Some other services include content marketing, branding, and graphic design.`,
     },
     {
-        Q: `Do you offer e-commerce web design?`,
-        A: `Yes, I definitely offer e-commerce website design and development services. I also specialize in creating and refining websites on Shopify or WordPress with the basic support of an on-site SEO service.`,
+        title: 'q2',
+        question: `Do you offer e-commerce web design?`,
+        answer: `Yes, I definitely offer e-commerce website design and development services. I also specialize in creating and refining websites on Shopify or WordPress with the basic support of an on-site SEO service.`,
     },
     {
-        Q: 'Do you offer SEO services?',
-        A: `Yes, I offer a range of SEO packages designed to meet your needs. My packages include strategies for both on-page and off-page optimization, such, as content creation, optimizing your web pages, building quality backlinks, and more. Whether you prefer my packages or need a customized solution, I am ready to assist you. Feel free to reach out to me today to discuss your requirements and begin enhancing your visibility.`,
+        title: 'q3',
+        question: 'Do you offer SEO services?',
+        answer: `Yes, I offer a range of SEO packages designed to meet your needs. My packages include strategies for both on-page and off-page optimization, such, as content creation, optimizing your web pages, building quality backlinks, and more. Whether you prefer my packages or need a customized solution, I am ready to assist you. Feel free to reach out to me today to discuss your requirements and begin enhancing your visibility.`,
     },
     {    
-        Q: `How long will a web development project take?`,
-        A: `Typically, my websites are completed within a timeframe of 2 to 10 weeks. However, I ensure that I have the capacity, for projects before accepting them to ensure your project will never be placed in a waiting queue.`,
+        title: 'q4',
+        question: `How long will a web development project take?`,
+        answer: `Typically, my websites are completed within a timeframe of 2 to 10 weeks. However, I ensure that I have the capacity, for projects before accepting them to ensure your project will never be placed in a waiting queue.`,
     },
     {    
-        Q: `How much will a web development project cost?`,
-        A: `Akanda's starting price for small and simple websites with 4-6 pages is approximately $450. As the number of pages, features, and complexity of the website grows, the range of pricing could be from $1,300 to more than $3,000. I also charge an hourly rate of $50 to $75/hr.`,
+        title: 'q5',
+        question: `How much will a web development project cost?`,
+        answer: `Akanda's starting price for small and simple websites with 4-6 pages is approximately $450. As the number of pages, features, and complexity of the website grows, the range of pricing could be from $1,300 to more than $3,000. I also charge an hourly rate of $50 to $75/hr.`,
     }
 
 ]
