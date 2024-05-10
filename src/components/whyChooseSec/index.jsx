d64035a5ee90f33c2bb45ee54c4874b6934447a8
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-10 13:40:27 +0100

    Create index.jsx

---
+++ src/components/whyChooseSec/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./whyChoose.module.css";
+
+export default function WhyChoose() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
