0723968bfec8be837a14b32945cb76699c1a8cc9
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-10 19:17:03 +0100

    Update index.jsx

--- src/components/homeAboutSec/index.jsx
+++ src/components/homeAboutSec/index.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import style from './HomeAboutSec.module.css'
 import { Body, Title } from '../text'
+import { HomeContent } from '@/webContents/homePage'
+import style from './HomeAboutSec.module.css'
 
 export default function HomeAboutSec() {
+
     return (
         <div className={style.wrap}>
             <div>
-                <Title title={'With a focus on the digital realm, Akanda specializes in website design and development, mobile app development, web analytics, and SEO.'}/>
+                <Title 
+                    title={HomeContent.introAbout} 
+                />
             </div>
             <div></div>
             <div>
-                <Body text={'Akanda forte is taking industry leaders forward with digital products that focuses and meet their wants and needs.'}/>
+                <Body text={HomeContent.about}/>
             </div>
         </div>
     )
