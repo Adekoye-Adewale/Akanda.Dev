c0496c2d7d9860aae3a52aaa10f992750409df52
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-10 13:42:12 +0100

    Update index.jsx

--- src/components/pages/HomePageComponents/index.jsx
+++ src/components/pages/HomePageComponents/index.jsx
@@ -2,14 +2,23 @@ import React from 'react'
 import { Body, SubTitle, Title } from '@/components/text'
 import Tab from '@/components/tab'
 import { HomeContent } from '@/webContents/homePage'
-import style from './homePage.module.css'
 import Accordion from '@/components/accordion'
 import { FAQ } from '@/webContents/faq'
 import SecBtn from '@/components/btn/secBtn'
+import SectionFoot from '@/components/sectionFooter'
+import HomeAboutSec from '@/components/homeAboutSec'
+import style from './homePage.module.css'
+import HomePortfolioSec from '@/components/homePortfolioSec'
 
 export default function HomePage() {
     return (
         <>
+            <section>
+                <HomeAboutSec/>
+            </section>
+            <section>
+                <HomePortfolioSec/>
+            </section>
             <section className={style.skill__sec}>
                 <div className='hidden'>
                     <Title title={"Akanda Skill"}/>
@@ -29,6 +38,9 @@ export default function HomePage() {
                 </div>
                 <Accordion Content={FAQ}/>
             </section>
+            <div className={style.sec__foot}>
+                <SectionFoot/>
+            </div>
         </>
     )
 }
