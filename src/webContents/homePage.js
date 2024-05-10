c52822dfe79a40d79ac172617131a4c796975ba7
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-10 13:42:28 +0100

    Update homePage.js

--- src/webContents/homePage.js
+++ src/webContents/homePage.js
@@ -2,6 +2,7 @@ export const HomeContent = {
     intro: `In the business of putting everyone online.`,
     introAbout: `With a focus on the digital realm, Akanda specializes in website design and development, mobile app development, analytics, and SEO.`,
     about: `Akanda forte is taking industry leaders forward with digital products that focus on and meet their wants and needs.`,
+    portfolioTitle: `Akanda.Dev work across many industries`,
     skillTitle: `As an experienced front-end software engineer with a keen eye for detail, Akanda is committed to providing exceptional quality on every project.`,
     skill: [
         {
