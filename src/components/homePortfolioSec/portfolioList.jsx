c7f9f48b91c35682f94a0d5ec50c07c4c944f8f1
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-10 23:29:04 +0100

    Update portfolioList.jsx

--- src/components/homePortfolioSec/portfolioList.jsx
+++ src/components/homePortfolioSec/portfolioList.jsx
@@ -1,22 +1,80 @@
-import React from 'react'
+'use client'
+import React,{ useRef} from 'react'
+import { useScroll, useTransform, motion} from 'framer-motion';
 import { Body, SubTitle } from '../text'
 import Image from 'next/image'
 import style from "./homePortfolioSec.module.css";
 
-export default function PortfolioList({ Works }) {
+
+export default function PortfolioList() {
+    const Works = [
+    {
+        client:`Mediatek NG`,
+        summary:`A media independent company`,
+        img: {
+            src: `/images/mediatelNG.webp`,
+            alt: `Mediatek NG-A media independent company`,
+            title: `Mediatek NG-A media independent company`,
+            width: `700`,
+            height: `409`,
+        },
+        url: {
+            href: `https://www.mediatekng.com/`,
+            title: `Click to visit Mediatek NG website`,
+        },
+    },
+    {
+        client:`Design Glaze`,
+        summary:`uPVC Windows and Doors Installer in United Kingdom`,
+        img: {
+            src: `/images/mediatelNG.webp`,
+            alt: `Design Glaze-uPVC Windows and Doors Installer in United Kingdom`,
+            title: `Design Glaze-uPVC Windows and Doors Installer in United Kingdom`,
+            width: `700`,
+            height: `409`,
+        },
+        url: {
+            href: `https://www.mediatekng.com/`,
+            title: `Click to visit Mediatek NG website`,
+        },
+    },
+]
+
     return (
         <div className={style.list__wrap}>
-            {Works.map((work, index) => (
-                <div key={index} className={style.list}>
+            {Works.map(({client, img, summary, url}) => (
+                <div key={client} className={style.list}>
                     <div className={style.copy__wrap}>
-                        <Body text={work.summary}/>
-                        <SubTitle subTitle={work.client}/>
+                        <Body text={summary}/>
+                        <SubTitle subTitle={client}/>
                     </div>
-                    <div className={style.img__wrap}>
-                        <Image {...work.img}/>
-                    </div>
+                    <GrowImg 
+                        img={img}
+                    />
                 </div>
             ))}
         </div>
+    )
+}
+
+
+const GrowImg = ({ img }) => {
+
+    const el = useRef(null);
+    const { scrollYProgress } = useScroll({
+        target: el,
+        offset: ['start 0.8', 'end start']
+    })
+
+    const scale = useTransform(scrollYProgress, [0, 0.5, 1], [0.8, 1.3, 0.5]);
+
+    return (
+        <motion.div 
+            ref={el}
+            style={{scale}}
+            className={style.img__wrap}
+        >
+            <Image {...img}/>
+        </motion.div>
     )
 }
