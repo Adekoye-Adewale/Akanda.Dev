f7062910ab14bd09e63d7b76ed1f320bee8666a3
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-10 13:39:47 +0100

    Create index.jsx

---
+++ src/components/homePortfolioSec/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Title } from '../text'
+import { HomeContent } from '@/webContents/homePage';
+import style from "./homePortfolioSec.module.css";
+import PortfolioList from './portfolioList';
+import { HomePortfolio } from '@/webContents/projects';
+
+
+export default function HomePortfolioSec() {
+    return (
+        <div className={style.wrap}>
+            <Title title={HomeContent.portfolioTitle}/>
+            <PortfolioList Works={HomePortfolio}/>
+        </div>
+    )
+}
