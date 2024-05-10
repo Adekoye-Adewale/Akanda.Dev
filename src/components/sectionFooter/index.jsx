350f3cb01a80aca3c57ff9ba183fd45513fedb79
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-10 13:42:19 +0100

    Update index.jsx

--- src/components/sectionFooter/index.jsx
+++ src/components/sectionFooter/index.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import Link from 'next/link'
 import { Body } from '../text'
+import style from './sectionFoot.module.css'
 
 export default function SectionFoot() {
     return (
-        <div>
-            <Link href={'/'}>
+        <div className={style.wrap}>
+            <Link 
+                href={'https://mobile.twitter.com/__akanda__'} 
+                target={'_blank'}
+                title={'Twitter Link'}
+            >
                 @__akanda__
             </Link>
             <Body text={"A Creative & Problem Solving Software Engineer From Nigeria"}/>
-            <Body text={'A Creator of Things On The WorldWideWeb'}/>
+            <Link 
+                href={''} 
+                target={'_blank'} 
+                title={'GitHub Link'}
+            >
+                A Creator of Things On The WorldWideWeb
+            </Link>
         </div>
     )
 }
