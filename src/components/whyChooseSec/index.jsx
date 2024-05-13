8eaffa119863f10620007bf4b296a81f1873796f
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-13 01:54:14 +0100

    Update index.jsx

--- src/components/whyChooseSec/index.jsx
+++ src/components/whyChooseSec/index.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import styles from "./whyChoose.module.css";
+import { Body, Title } from '../text'
+import style from "./whyChoose.module.css"
+import Image from 'next/image'
 
-export default function WhyChoose() {
+export default function WhyChoose({ Why, desc }) {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Title title={'Why choose Akanda.Dev?'}/>
+            <div className={style.wrap}>
+                <div className={style.copy__wrap}>
+                    <Body text={desc}/>
+                </div>
+                <div className={style.list__wrap}>
+                    {Why.map((choose, index)=>(
+                        <span key={index}  className={style.list}>
+                            {choose.title}
+                            <Image {...choose.img}/>
+                        </span>
+                    ))}
+                </div>
+            </div>
+        </>
     )
 }
