057318c804c0fbf32d1138e0025189b36719ad11
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-14 22:42:06 +0100

    Update HeroSection.jsx

--- src/components/pages/ContactPageContent/HeroSection.jsx
+++ src/components/pages/ContactPageContent/HeroSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Heading } from '@/components/text'
 import styles from './contactPageContent.module.css'
+import PriBtn from '@/components/btn/priBtn'
 
 const ContactHero = `<span class='hello italic'>Hello</span><span>Let's Connect, Meet,</span><span>Drink, and Work Together.</span>`
 
@@ -8,6 +9,7 @@ export default function Hero() {
     return (
         <div className={`${styles.hero__wrapper}`}>
             <Heading head={ContactHero}/>
+            <PriBtn text={'Arrange a meeting'} href={'/'}/>
         </div>
     )
 }
