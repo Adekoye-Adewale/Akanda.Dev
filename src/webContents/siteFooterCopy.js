b0c6a374b4009ab930c37a6c120d2c3e80dbb6ec
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-14 11:53:24 +0100

    Update siteFooterCopy.js

--- src/webContents/siteFooterCopy.js
+++ src/webContents/siteFooterCopy.js
@@ -2,16 +2,19 @@ export const FootCard = [
     {
         pri: `View my`,
         sec: `Projects`,
+        desc: `See my best projects and the stories behind them.`,
         link: `/website-development-projects`,
     },
     {
         pri: `Meet Akanda`,
         sec: `Contact`,
+        desc: `Let's collaborate to boost your revenue like never before.`,
         link: `/contact-website-developer-in-lagos`,
     },
     {
         pri: `Learn more`,
         sec: `About me`,
+        desc: `See what makes me a standout software engineer.`,
         link: `/about-akanda`,
     },
 ]
