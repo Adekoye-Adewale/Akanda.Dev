7717e3abe887c66605826d72c0548a2a412e9763
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-15 02:44:41 +0100

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,10 @@
-import HomeHero from "@/components/hero/HomeHero";
 import HomePage from "@/components/pages/HomePageComponents";
-import styles from "./page.module.css";
 
 export default function Home() {
 
   return (
-    <main className={styles.main}>
-      <HomeHero/>
+    <>
       <HomePage/>
-    </main>
+    </>
   );
 }
