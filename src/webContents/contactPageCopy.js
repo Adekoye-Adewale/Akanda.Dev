c6e9d7d6e976f918184c2113f28cea4c0def8dda
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-16 21:19:11 +0100

    Create contactPageCopy.js

---
+++ src/webContents/contactPageCopy.js
@@ -0,0 +1,50 @@
+export const ContactPageCopy = {
+    hero: `<span class='hello italic'>Hello </span><span>Let's Connect, Meet,</span><span>Drink, and Work Together.</span>`,
+    link: {
+        text: `Arrange a meeting`,
+        href: `/`,
+    },
+}
+
+export const ContactInfoCopy = {
+    Lists: [
+        {
+            title: `Email`,
+            text: `info@akanda.dev`,
+            link: {
+                href: `mailto:info@akanda.dev`,
+                title: `Akanda Dev business email`
+            }
+        },
+        {
+            title: `Telephone`,
+            text: `+234 806 7355 472`,
+            link: {
+                href: `tel:+2348067355472`,
+                title: `Akanda Dev business number`
+            }
+        },
+    ],
+    title: `Connect via Social media`,
+    Socials: [
+        {
+            url: {
+                href: `/`,
+                title: `AkandaDev YouTube Channel Link`
+            },
+            img: {
+                src: `/images/ai-icon.png`,
+                alt: `AkandaDev YouTube Channel Link`,
+                height: `36`,
+                width: `36`,
+            }
+        }
+    ],
+    text: `Ready to take your digital presence to the next level? Let's connect and discuss how Akanda.Dev can help you achieve your goals.`
+}
+
+export const ContactCTACopy = {
+    title: `You need a meeting with Akanda today!`,
+    text: `I can help you achieve your goals, whether they involve improving the look and feel of your brand’s website, revamping your website, or strengthening your SEO plan. Reach out to Akanda Dev today to schedule a consultation and start your journey toward digital success.`,
+    btnText: `Make an appointment`,
+}
