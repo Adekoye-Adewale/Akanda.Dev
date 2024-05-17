261fc5cda012c07c38217e5e5c39656794efe9eb
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-17 22:40:36 +0100

    Create index.js

---
+++ src/app/_schema/index.js
@@ -0,0 +1,57 @@
+export const Home = {
+  "@context": "https://schema.org",
+  "@type": "FAQPage",
+  mainEntity: [
+    {
+      "@type": "Question",
+      name: "What services does Akanda offer?",
+      acceptedAnswer: {
+        "@type": "Answer",
+        text: "Akanda offers a variety of services, but his main areas of focus are web design and development, mobile applications, digital transformation consulting services, and SEO. Some other services include content marketing, branding, and graphic design.",
+      },
+    },
+    {
+      "@type": "Question",
+      name: "Does Akanda Dev offer e-commerce website development?",
+      acceptedAnswer: {
+        "@type": "Answer",
+        text: "Yes, I definitely offer e-commerce website design and development services. I also specialize in creating and refining websites on Shopify or WordPress with the basic support of an on-site SEO service.",
+      },
+    },
+    {
+      "@type": "Question",
+      name: "Does Akanda Dev offer SEO services?",
+      acceptedAnswer: {
+        "@type": "Answer",
+        text: "Yes, Akanda offers a range of SEO packages designed to meet your needs. My packages include strategies for both on-page and off-page optimization, such, as content creation, optimizing your web pages, building quality backlinks, and more. Whether you prefer my packages or need a customized solution, I am ready to assist you. Feel free to reach out to me today to discuss your requirements and begin enhancing your visibility.",
+      },
+    },
+    {
+      "@type": "Question",
+      name: "How long will a web development project take?",
+      acceptedAnswer: {
+        "@type": "Answer",
+        text: "Typically, my websites are completed within the timeframe of 2 to 10 weeks.",
+      },
+    },
+    {
+      "@type": "Question",
+      name: "How much will a web development project cost?",
+      acceptedAnswer: {
+        "@type": "Answer",
+        text: "Akanda's starting price for small and simple websites with 4-6 pages is approximately $450. As the number of pages, features, and complexity of the website grows, the range of pricing could be from $1,300 to more than $3,000. Akanda Dev also charges an hourly rate between $50 to $75/hr.",
+      },
+    },
+  ],
+  headline: "Frequently Asked Questions about Akanda",
+  description:
+    "Akanda forte is taking industry leaders forward with digital products that focus on and meet their wants and needs.",
+  author: {
+    "@type": "Person",
+    name: "Adekoye Adewale",
+    url: "https://adekoyeadewale.com",
+  },
+  image: "/akanda_dev.png",
+  datePublished: "2024-04-20",
+  dateModified: "2023-05-17",
+};
