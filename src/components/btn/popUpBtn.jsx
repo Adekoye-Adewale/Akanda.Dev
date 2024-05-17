f534709d3233b93b12039d3474d72b1ac62e5cb1
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-17 12:08:43 +0100

    Update popUpBtn.jsx

--- src/components/btn/popUpBtn.jsx
+++ src/components/btn/popUpBtn.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
+import style from './btn.module.css'
 
 export default function PopUpBtn({ text, onClick }) {
     return (
         <button 
-            onClick={onClick} 
-            data-text={text}
+            className={style.button}
+            onClick={onClick}
         >
-            {text}
+            <div 
+                className={style.el}
+            >
+                <PerspectiveText
+                    label={text}
+                />
+            </div>
+            <div 
+                className={style.el}
+            >
+                <PerspectiveText
+                    label={text}
+                />
+            </div>
         </button>
+    )
+}
+
+function PerspectiveText({label}) {
+    return (    
+        <div className={style.perspectiveText}>
+            <p>{label}</p>
+            <p>{label}</p>
+        </div>
     )
 }
