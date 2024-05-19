c4fae900de5e630c19b2db1c4d8edd83cde451d9
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-19 16:05:18 +0100

    Update HomeHero.jsx

--- src/components/hero/HomeHero.jsx
+++ src/components/hero/HomeHero.jsx
@@ -9,11 +9,19 @@ const homeHero = `<span class='italic'>In Business</span> <span>of making everyo
 export default function HomeHero({scrollYProgress}) {
 
     const scale = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
-    const rotate = useTransform(scrollYProgress, [0, 1], [20, 0]);
+    // const rotate = useTransform(scrollYProgress, [0, 1], [20, 0]);
     const opacity = useTransform(scrollYProgress, [0, 1], [0.3, 1]);
     
     return (
-        <motion.div className={`${styles.wrapper}`} style={{scale, rotate, opacity}} >
+        <motion.div 
+            className={`${styles.wrapper}`} 
+            style={{scale, opacity}} 
+            transition={{ 
+                duration: 0.2, 
+                type: "tween",
+                ease: "easeInOut"
+            }}
+        >
             <Heading head={homeHero}/>
         </motion.div>
     )
