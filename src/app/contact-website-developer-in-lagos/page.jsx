695b4de96821a6cadcf4d0537a3873ddde964f42
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-20 01:02:55 +0100

    Update page.jsx

--- src/app/contact-website-developer-in-lagos/page.jsx
+++ src/app/contact-website-developer-in-lagos/page.jsx
@@ -1,5 +1,13 @@
 import ContactPageContent from "@/components/pages/ContactPageContent";
 
+export const metadata = {
+    title: "Contact Akanda Dev",
+    alternates: {
+        canonical: `/contact-website-developer-in-lagos`,
+      },
+};
+
+
 export default function ContactPage() {
     
     return (
