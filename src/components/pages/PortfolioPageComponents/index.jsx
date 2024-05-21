f8fe94743592e82603be1445b4b88fd1405f7910
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-21 20:03:34 +0100

    Create index.jsx

---
+++ src/components/pages/PortfolioPageComponents/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { HeroSection } from '../HomePageComponents'
+import { portfolioHero } from '@/webContents/portfolioPageCopy'
+import SectionFoot from '@/components/sectionFooter'
+import SectionRotate from '@/components/util/sectionRotate'
+import PortfolioLists from '@/components/portfolioLists'
+import CtaWrap from '@/components/siteFooter/ctaWrap'
+
+export default function PortfolioContents() {
+    return (
+        <main>
+            <HeroSection 
+                className={`portfolio__hero`}
+                Hero={portfolioHero}
+            />
+            <SectionRotate>
+                <PortfolioLists/>
+            </SectionRotate>
+            <div className={`sec__foot`}>
+                <SectionFoot/>
+            </div>
+            <CtaWrap/>
+        </main>
+    )
+}
