96f12a8dfd39e169539a9beb8f83acf6f7f0ea83
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-21 20:04:01 +0100

    Update page.jsx

--- src/app/website-development-projects/page.jsx
+++ src/app/website-development-projects/page.jsx
@@ -1,10 +1,10 @@
+import PortfolioContents from "@/components/pages/PortfolioPageComponents";
+
 export default function ProjectPage() {
     
     return (
-        <div className='grid__center full__size'>
-            <h1>
-                Projects
-            </h1>
-        </div>
+        <>
+            <PortfolioContents/>
+        </>
     )
 }
