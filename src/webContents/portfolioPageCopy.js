70244c25bff605f2a501a921e92273a68be0b138
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-21 20:03:43 +0100

    Create portfolioPageCopy.js

---
+++ src/webContents/portfolioPageCopy.js
@@ -0,0 +1,245 @@
+export const portfolioHero = `<span>Projects crafted</span> <span>for my clients turn</span> <span>friends and family😉</span>`;
+
+export const portfolioList = [
+    {
+        id: `0001`,
+        title: `Mediatek NG`,
+        type: `Website Design and Dev`,
+        img: {
+            src: '/images/mediatek.png',
+            alt: 'Mediatek NG - Website Design and Development',
+            title: `Mediatek NG - Website Design and Development`,
+            height: 203,
+            width: 300,
+        },
+        slug: {
+            href: '/mediatek',
+            title: `View more`,
+        }
+    },
+    {
+        id: `0002`,
+        title: `BL Brand`,
+        type: `Website Design and Dev`,
+        img: {
+            src: '/images/mediatek.png',
+            alt: 'Mediatek NG - Website Design and Development',
+            title: `Mediatek NG - Website Design and Development`,
+            height: 203,
+            width: 300,
+        },
+        slug: {
+            href: '/bl-brand',
+            title: `View more`,
+        }
+    },
+    {
+        id: `0003`,
+        title: `Design Glaze`,
+        type: `Ads & Campaign Setup`,
+        img: {
+            src: '/images/mediatek.png',
+            alt: 'Mediatek NG - Website Design and Development',
+            title: `Mediatek NG - Website Design and Development`,
+            height: 203,
+            width: 300,
+        },
+        slug: {
+            href: '/design-glaze',
+            title: `View more`,
+        }
+    },
+    {
+        id: `0004`,
+        title: `SMC Premier Group`,
+        type: `SEO`,
+        img: {
+            src: '/images/mediatek.png',
+            alt: 'Mediatek NG - Website Design and Development',
+            title: `Mediatek NG - Website Design and Development`,
+            height: 203,
+            width: 300,
+        },
+        slug: {
+            href: '/smc-premier-group',
+            title: `View more`,
+        }
+    },
+    {
+        id: `0005`,
+        title: `Reset USA`,
+        type: `Website Design and Dev`,
+        img: {
+            src: '/images/mediatek.png',
+            alt: 'Mediatek NG - Website Design and Development',
+            title: `Mediatek NG - Website Design and Development`,
+            height: 203,
+            width: 300,
+        },
+        slug: {
+            href: '/reset-usa',
+            title: `View more`,
+        }
+    },
+    {
+        id: `0006`,
+        title: `Mediatek NG`,
+        type: `Website Design and Dev`,
+        img: {
+            src: '/images/mediatek.png',
+            alt: 'Mediatek NG - Website Design and Development',
+            title: `Mediatek NG - Website Design and Development`,
+            height: 203,
+            width: 300,
+        },
+        slug: {
+            href: '/bl-brand',
+            title: `View more`,
+        }
+    },
+    {
+        id: `0007`,
+        title: `Mediatek NG`,
+        type: `Website Design and Dev`,
+        img: {
+            src: '/images/mediatek.png',
+            alt: 'Mediatek NG - Website Design and Development',
+            title: `Mediatek NG - Website Design and Development`,
+            height: 203,
+            width: 300,
+        },
+        slug: {
+            href: '/bl-brand',
+            title: `View more`,
+        }
+    },
+    {
+        id: `0008`,
+        title: `Mediatek NG`,
+        type: `Website Design and Dev`,
+        img: {
+            src: '/images/mediatek.png',
+            alt: 'Mediatek NG - Website Design and Development',
+            title: `Mediatek NG - Website Design and Development`,
+            height: 203,
+            width: 300,
+        },
+        slug: {
+            href: '/bl-brand',
+            title: `View more`,
+        }
+    },
+    {
+        id: `0009`,
+        title: `Mediatek NG`,
+        type: `Website Design and Dev`,
+        img: {
+            src: '/images/mediatek.png',
+            alt: 'Mediatek NG - Website Design and Development',
+            title: `Mediatek NG - Website Design and Development`,
+            height: 203,
+            width: 300,
+        },
+        slug: {
+            href: '/bl-brand',
+            title: `View more`,
+        }
+    },
+    {
+        id: `0010`,
+        title: `Mediatek NG`,
+        type: `Website Design and Dev`,
+        img: {
+            src: '/images/mediatek.png',
+            alt: 'Mediatek NG - Website Design and Development',
+            title: `Mediatek NG - Website Design and Development`,
+            height: 203,
+            width: 300,
+        },
+        slug: {
+            href: '/bl-brand',
+            title: `View more`,
+        }
+    },
+    {
+        id: `0011`,
+        title: `Mediatek NG`,
+        type: `Website Design and Dev`,
+        img: {
+            src: '/images/mediatek.png',
+            alt: 'Mediatek NG - Website Design and Development',
+            title: `Mediatek NG - Website Design and Development`,
+            height: 203,
+            width: 300,
+        },
+        slug: {
+            href: '/bl-brand',
+            title: `View more`,
+        }
+    },
+    {
+        id: `0012`,
+        title: `Mediatek NG`,
+        type: `Website Design and Dev`,
+        img: {
+            src: '/images/mediatek.png',
+            alt: 'Mediatek NG - Website Design and Development',
+            title: `Mediatek NG - Website Design and Development`,
+            height: 203,
+            width: 300,
+        },
+        slug: {
+            href: '/bl-brand',
+            title: `View more`,
+        }
+    },
+    {
+        id: `0013`,
+        title: `Mediatek NG`,
+        type: `Website Design and Dev`,
+        img: {
+            src: '/images/mediatek.png',
+            alt: 'Mediatek NG - Website Design and Development',
+            title: `Mediatek NG - Website Design and Development`,
+            height: 203,
+            width: 300,
+        },
+        slug: {
+            href: '/bl-brand',
+            title: `View more`,
+        }
+    },
+    {
+        id: `0014`,
+        title: `Mediatek NG`,
+        type: `Website Design and Dev`,
+        img: {
+            src: '/images/mediatek.png',
+            alt: 'Mediatek NG - Website Design and Development',
+            title: `Mediatek NG - Website Design and Development`,
+            height: 203,
+            width: 300,
+        },
+        slug: {
+            href: '/bl-brand',
+            title: `View more`,
+        }
+    },
+    {
+        id: `0015`,
+        title: `Mediatek NG`,
+        type: `Website Design and Dev`,
+        img: {
+            src: '/images/mediatek.png',
+            alt: 'Mediatek NG - Website Design and Development',
+            title: `Mediatek NG - Website Design and Development`,
+            height: 203,
+            width: 300,
+        },
+        slug: {
+            href: '/bl-brand',
+            title: `View more`,
+        }
+    },
+]
+
