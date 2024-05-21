a64687d9289ccf591f89f3767fb5c9a03ac6f5fd
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-21 20:03:31 +0100

    Create cards.jsx

---
+++ src/components/portfolioLists/cards.jsx
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+import style from './portfolioLists.module.css'
+import Link from 'next/link';
+
+export default function Cards({ CardList }) {
+    return (
+        <div className={style.parent__wrap}>
+            {CardList.map((list, index) => (
+                <Link 
+                    {...list.slug} 
+                    key={index} 
+                    className={style.wrap}
+                >
+                    <Card 
+                        img={list.img}
+                        title={list.title}
+                        type={list.type}
+                    />
+                </Link>
+            ))}
+        </div>
+    )
+};
+
+
+export const Card = ({ img, title, type }) => {
+    return (
+        <>
+            <div className={style.img__wrap}>
+                <Image {...img} />    
+            </div>
+            <div className={style.copy__wrap}>
+                <span>{title}</span>
+                <h4>{type}</h4>
+            </div>
+        </>
+    );
+}
