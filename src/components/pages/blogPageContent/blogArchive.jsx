dfd1f1e7288881d05b523245090681f9affb6f88
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-22 20:18:20 +0100

    Create blogArchive.jsx

---
+++ src/components/pages/blogPageContent/blogArchive.jsx
@@ -0,0 +1,12 @@
+import ArchiveHero from '@/components/blog/hero'
+import CtaWrap from '@/components/siteFooter/ctaWrap'
+import React from 'react'
+
+export default function BlogArchive() {
+    return (
+        <main>
+            <ArchiveHero/>
+            <CtaWrap/>
+        </main>
+    )
+}
