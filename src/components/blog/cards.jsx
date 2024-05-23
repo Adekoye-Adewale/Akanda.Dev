3ce27850307f44a51544d8b7808dd007526363eb
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-23 22:33:55 +0100

    Create cards.jsx

---
+++ src/components/blog/cards.jsx
@@ -0,0 +1,41 @@
+import Image from "next/image"
+import { articlePageCopy } from "@/webContents/blogCopy"
+import style from './blog.module.css'
+import Link from "next/link";
+
+export default function Cards() {
+    const Content = articlePageCopy;
+    return (
+        <div className={style.archive__cards}>
+            {Content.map (( card ) => (
+                <Link 
+                    href={card.slug}
+                    title={card.title}
+                    target="__blank"
+                    className={style.archive__card}
+                    key={card.id}
+                >
+                    <Card 
+                        img={card.img} 
+                        title={card.title}
+                    />
+                </Link>
+            ))}
+        </div>
+    )
+}
+
+const Card = ({ img, title }) => {
+    return (
+        <>
+            <div 
+                className={`${style.archive__card__img__wrap}`}
+            >
+                 <Image {...img}/>
+            </div>
+            <span className={style.archive__card__title}>
+                {title}
+            </span>
+        </>
+    )
+}
