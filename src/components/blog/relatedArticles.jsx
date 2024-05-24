8af5e791d6086b6818914b1f1739f11f89ab103c
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-24 22:08:16 +0100

    Update relatedArticles.jsx

--- src/components/blog/relatedArticles.jsx
+++ src/components/blog/relatedArticles.jsx
@@ -1,10 +1,38 @@
-import { SubTitle } from '../text'
-import Image from 'next/image'
-import style from './blog.module.css'
-import { articlePageCopy } from '@/webContents/blogCopy'
+'use client'
+import React from 'react';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
+import { SubTitle } from '../text';
+import Image from 'next/image';
+import { articlePageCopy } from '@/webContents/blogCopy';
+import '@splidejs/react-splide/css/core';
+import style from './blog.module.css';
 
 export default function RelatedArticles() {
     const Related = articlePageCopy;
+    const options = {
+        type: 'loop',
+        drag: 'free',
+        // wheel: true,
+        snap: true,
+        gap: '2rem',
+        perPage: 3,
+        perMove: 1,
+        autoWidth: 'true',
+        cover: true,
+        lazyLoad: 'nearby',
+        breakpoints: {
+            800: {
+              perPage: 2,
+              gap: '1rem',
+              autoHeight : 'true',
+            },
+        },
+        autoplay: true,
+        autoScroll: {
+            speed: 1,
+        },
+    };
     
     return (
         <div className={style.related__section__wrap}>
@@ -12,13 +40,15 @@ export default function RelatedArticles() {
                 <SubTitle subTitle={'Related Articles'}/>
             </div>
             <div className={style.related__cards__wrap}>
-                {Related.slice(0, 3).map((card) => (
-                    <Card 
-                        key={card.id}
-                        title={card.title} 
-                        img={card.img}
-                    />
-                ))}
+                <Splide options={options} autoStart={{boolean: true}}>
+                    {Related.slice(0, 3).map((card) => (
+                        <Card 
+                            key={card.id}
+                            title={card.title} 
+                            img={card.img}
+                        />
+                    ))}
+                </Splide>
             </div>
         </div>
     )
@@ -26,13 +56,16 @@ export default function RelatedArticles() {
 
 const Card = ({ key, title, img }) => {
     return (
-        <div className={style.related__articles__wrap} key={key}>
+        <SplideSlide 
+            className={style.related__articles__wrap} 
+            key={key}
+        >
             <div className={style.related__articles__img__wrap}>
                 <Image {...img}/>
             </div>
             <span className={style.related__articles__title}>
                 {title}
             </span>
-        </div>
+        </SplideSlide>
     )
 }
