6eda765d5ece23dedeff2170f3ec8e34c0ad3f3f
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-26 18:44:22 +0100

    Update contentful.js

--- src/app/api/contentful.js
+++ src/app/api/contentful.js
@@ -1,6 +1,10 @@
-// import * as Contentful from Contentful
+import * as Contentful from "contentful";
 
-// export var client = Contentful.createClient({
-//     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-//     space: process.env.CONTENTFUL_SPACE_ID,
-// })
+export const client = Contentful.createClient({
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    space: process.env.CONTENTFUL_SPACE_ID,
+});
+
+const response = await client.getEntry(process.env.ENTRY_ID);
+export const articleContents = await client.getEntries({content_type: 'blog',});
+// console.log('ENtAdi:',articleContents.items)
