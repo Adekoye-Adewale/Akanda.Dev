87009a2fa3988a863f288d5f138e99a7851096c8
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-27 18:29:59 +0100

    Update blogArchive.jsx

--- src/components/pages/blogPageContent/blogArchive.jsx
+++ src/components/pages/blogPageContent/blogArchive.jsx
@@ -1,13 +1,18 @@
 import Cards from '@/components/blog/cards'
 import ArchiveHero from '@/components/blog/hero'
 import CtaWrap from '@/components/siteFooter/ctaWrap'
+import { articlePageCopy } from "@/webContents/blogCopy"
 
 export default function BlogArchive() {
     return (
         <main>
             <ArchiveHero/>
-            <section className='container__pad full__size'>
-                <Cards/>
+            <section 
+                className='container__pad full__size'
+            >
+                <Cards 
+                    Contents={articlePageCopy}
+                />
             </section>
             <CtaWrap/>
         </main>
