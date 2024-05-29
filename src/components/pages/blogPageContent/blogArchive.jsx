88e88c1eccc867ddaaff045fc86c33998588f1cb
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-29 22:11:25 +0100

    Update blogArchive.jsx

--- src/components/pages/blogPageContent/blogArchive.jsx
+++ src/components/pages/blogPageContent/blogArchive.jsx
@@ -2,13 +2,31 @@ import Cards from '@/components/blog/cards'
 import ArchiveHero from '@/components/blog/hero'
 import CtaWrap from '@/components/siteFooter/ctaWrap'
 import { articlePageCopy } from "@/webContents/blogCopy"
-import { articleContents } from '@/app/api/contentful'
 
 export default function BlogArchive() {
 
-    const blogContent = articleContents.items
+    // console.log('first:', blogContent);
 
-    console.log('first:', blogContent);
+    // const ContentTest = blogContent?.map((content) => {
+    //     const fields = content?.fields;
+    //     const img = fields?.img;
+    //     const system = content?.sys?.id
+    
+    //     return {
+    //         id: system,
+    //         title: fields?.title,
+    //         slug: fields?.slug,
+    //         img: img ? {
+    //             src: img.fields.file.url.replace('//', 'https://'),
+    //             alt: img.fields.description || '',
+    //             title: img.fields.title || '',
+    //             height: img.fields.file.details.image.height,
+    //             width: img.fields.file.details.image.width,
+    //         } : null,
+    //     };
+    // }) || [];
+
+    // console.log('first:', ContentTest);
 
     return (
         <main>
