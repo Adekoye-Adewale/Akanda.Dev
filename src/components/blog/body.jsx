d833cff9da5b8e0ab7626f5367bdea403414c7f8
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-30 01:19:10 +0100

    Update body.jsx

--- src/components/blog/body.jsx
+++ src/components/blog/body.jsx
@@ -1,12 +1,16 @@
 import style from './blog.module.css'
 import BlogArticle from './blogArticle'
-import RelatedArticles from './relatedArticles'
+import dynamic from 'next/dynamic';
+
+const RelatedArticles = dynamic(() => import('./relatedArticles'), {
+    ssr: false,
+});
 
 export default function BlogBody({ blog, related }) {
     return (
         <div className={style.blog__body}>
             <BlogArticle blog={blog}/>
-            {/* <RelatedArticles Related={related}/> */}
+            {related.length > 0 && <RelatedArticles related={related} />}
         </div>
     )
 }
