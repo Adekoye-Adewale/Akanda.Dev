cffa16c7b35e1fcfc40cd030a7267cc5cf8a3b70
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-30 21:18:41 +0100

    Create introSec.jsx

---
+++ src/components/blog/introSec.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import style from './blog.module.css'
+
+export default function IntroSec() {
+    return (
+        <div className={style.intro__sec}>
+            <div className={style.source}>
+                <span></span>
+                <span></span>
+            </div>
+            
+            <div>
+                <span>
+
+                </span>
+            </div>
+        </div>
+    )
+}
