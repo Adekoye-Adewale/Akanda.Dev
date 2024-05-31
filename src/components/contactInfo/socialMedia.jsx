8ec55f2141579caefc6ea52ed57babd6accd9fd8
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-31 20:48:55 +0100

    Update socialMedia.jsx

--- src/components/contactInfo/socialMedia.jsx
+++ src/components/contactInfo/socialMedia.jsx
@@ -4,11 +4,15 @@ import Link from 'next/link'
 import Image from 'next/image'
 import Magnetic from '../magnetic'
 import style from './contactInfo.module.css'
+import { SocialIcon } from '../socials'
 
 export default function SocialMedia({ title, Socials }) {
     return (
         <div className={style.social}>
             <Title title={title}/>
+            <div>
+                <SocialIcon/>
+            </div>
             <div>
                 {Socials.map(( social, index ) => (
                     <div key={index}>
