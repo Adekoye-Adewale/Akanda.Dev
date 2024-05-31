1f2531ac9b43feb95bed0da11cf7c1f81e083cfb
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-05-31 20:49:07 +0100

    Create index.jsx

---
+++ src/components/socials/index.jsx
@@ -0,0 +1,4 @@
+import { X, Facebook, LinkedIn, Medium, Email, PrintPage, ShareLink } from './icons'
+import SocialIcon from './socialMediaIcons';
+
+export { X, Facebook, LinkedIn, Medium, Email, PrintPage, ShareLink, SocialIcon };
