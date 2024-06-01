2cdd206b765acf2d4b074b321263e19a45c4083e
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-06-01 12:10:13 +0100

    Update singleBlogPage.jsx

--- src/components/pages/blogPageContent/singleBlogPage.jsx
+++ src/components/pages/blogPageContent/singleBlogPage.jsx
@@ -4,6 +4,7 @@ import { articlePageCopy } from '@/webContents/blogCopy';
 import { BlogHero } from '@/components/blog/hero';
 import BlogBody from "@/components/blog/body";
 import CtaWrap from '@/components/siteFooter/ctaWrap';
+import Interact from '@/components/blog/interact';
 
 
 export default function SingleBlogPage({ params }) {
@@ -17,12 +18,14 @@ export default function SingleBlogPage({ params }) {
         return null; 
     }
 
-    const { img, title: blogTitle, type, category, datePublished, articleCopy, id } = blogContent;
+    const { img, title: blogTitle, type, category, datePublished, articleCopy, id, sourceLink, articleSource } = blogContent;
     const contentCopy = documentToReactComponents(articleCopy);
 
     const relatedArticles = articlePageCopy.filter(
         (content) => content.category === category && content.id !== id
     );
+
+    console.log(relatedArticles)
 
     return (
         <main>
@@ -32,6 +35,9 @@ export default function SingleBlogPage({ params }) {
                 type={type} 
                 category={category} 
                 date={datePublished}
+            />
+            <Interact 
+                {...blogContent}
             />
             <BlogBody 
                 blog={contentCopy}
