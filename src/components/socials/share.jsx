d3eb5be5b6d256bdb639fcf229427a69ac918582
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-06-01 12:10:18 +0100

    Update share.jsx

--- src/components/socials/share.jsx
+++ src/components/socials/share.jsx
@@ -1,13 +1,14 @@
 import { X, Facebook, LinkedIn, Medium, Email, ShareLink, PrintPage } from "."
+import style from './socialIcons.module.css'
 
-export default function Share() {
+export default function Share({ title }) {
     return (
         <div className={style.icons__wrap}>
             <X/>
             <LinkedIn/>
             <Facebook/>
             <Medium/>
-            <Email/>
+            <Email title={title}/>
             <PrintPage/>
             <ShareLink/>
         </div>
