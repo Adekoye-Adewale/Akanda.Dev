0be36d0a0c786ccc22191462f7382965f88495d5
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-06-03 22:34:08 +0100

    Create socialLinks.js

---
+++ src/webContents/socialLinks.js
@@ -0,0 +1,11 @@
+export const fbLink = {
+    href: ``,
+    title: `facebook link`,
+    target: `_blank`,
+}
+
+export const xLink = {
+    href: `https://twitter.com/__akanda__`,
+    title: `facebook link`,
+    target: `_blank`,
+}
