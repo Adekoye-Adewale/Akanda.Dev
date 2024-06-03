a26dc9c7273024de89c5da993b74be44b11f963c
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-06-03 22:35:57 +0100

    Update socialLinks.js

--- src/webContents/socialLinks.js
+++ src/webContents/socialLinks.js
@@ -9,3 +9,21 @@ export const xLink = {
     title: `facebook link`,
     target: `_blank`,
 }
+
+export const linkedinLink = {
+    href: ``,
+    title: `facebook link`,
+    target: `_blank`,
+}
+
+export const mailLink = {
+    href: `https://twitter.com/__akanda__`,
+    title: `facebook link`,
+    target: `_blank`,
+}
+
+export const mediumLink = {
+    href: `https://twitter.com/__akanda__`,
+    title: `facebook link`,
+    target: `_blank`,
+}
