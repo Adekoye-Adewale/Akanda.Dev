48cb18940fb32543317bbfa924925a94189de990
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-06-05 18:58:32 +0100

    Update HomeHero.jsx

--- src/components/hero/HomeHero.jsx
+++ src/components/hero/HomeHero.jsx
@@ -6,18 +6,18 @@ import styles from './hero.module.css'
 
 export default function HomeHero({ scrollYProgress, Hero }) {
 
-    const scale = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
-    const opacity = useTransform(scrollYProgress, [0, 1], [0.3, 1]);
+    // const scale = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
+    // const opacity = useTransform(scrollYProgress, [0, 1], [0.3, 1]);
     
     return (
         <motion.div 
             className={`${styles.wrapper}`} 
-            style={{scale, opacity}} 
-            transition={{ 
-                duration: 0.2, 
-                type: "tween",
-                ease: "easeInOut"
-            }}
+            // style={{scale, opacity}} 
+            // transition={{ 
+            //     duration: 0.2, 
+            //     type: "tween",
+            //     ease: "easeInOut"
+            // }}
         >
             <Heading head={Hero}/>
         </motion.div>
