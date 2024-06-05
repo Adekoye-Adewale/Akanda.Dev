dbdbe6a3673bdce4f890acd101bfd1da9fdf0933
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-06-05 18:58:43 +0100

    Update blogArchive.jsx

--- src/components/pages/blogPageContent/blogArchive.jsx
+++ src/components/pages/blogPageContent/blogArchive.jsx
@@ -5,29 +5,6 @@ import { articlePageCopy } from '@/webContents/blogCopy'
 
 export default function BlogArchive() {
 
-    // console.log('first:', blogContent);
-
-    // const ContentTest = blogContent?.map((content) => {
-    //     const fields = content?.fields;
-    //     const img = fields?.img;
-    //     const system = content?.sys?.id
-    
-    //     return {
-    //         id: system,
-    //         title: fields?.title,
-    //         slug: fields?.slug,
-    //         img: img ? {
-    //             src: img.fields.file.url.replace('//', 'https://'),
-    //             alt: img.fields.description || '',
-    //             title: img.fields.title || '',
-    //             height: img.fields.file.details.image.height,
-    //             width: img.fields.file.details.image.width,
-    //         } : null,
-    //     };
-    // }) || [];
-
-    // console.log('first:', ContentTest);
-
     return (
         <main>
             <ArchiveHero/>
