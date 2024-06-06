d31bb4e504798ac20a01b6173ee0ee850138f50b
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-06-06 15:35:35 +0100

    Update index.jsx

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -7,16 +7,14 @@ import styles from './header.module.css';
 const HeaderComp = forwardRef(function index(props, ref) {
   return (
     <header className={styles.header}>
-      <div className='img__wrap'>
+      <div className={`img__wrap ${styles.logo__wrap}`}>
         <Logo/>
       </div>
-      <div className={styles.menu__button}>
+      <div className={`${styles.menu__button} ${styles.menu__wrap}`}>
         <NavMenu/>
-        <Magnetic>
-            <div className={styles.burger}>
-                <div ref={ref} className={styles.bounds}></div>
-            </div>
-        </Magnetic>
+        <div className={styles.burger}>
+            <div ref={ref} className={styles.bounds}></div>
+        </div>
       </div>
     </header>
   )}
