641fb404578498667e86825146a848fe8e450b4a
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-06-06 01:47:08 +0100

    Create getBackgroundColor.js

---
+++ src/components/portfolioLists/getBackgroundColor.js
@@ -0,0 +1,22 @@
+export const getBackgroundColor = (type) => {
+    switch (type) {
+        // case 'Landing Page':
+        //     return '#171da3';
+        // case 'Corporate Website':
+        //     return '#564505';
+        // case 'Property Listing':
+        //     return '#AC32CB';
+        // case 'Sales Page':
+        //     return '#4d0f0f';
+        // case 'Portfolio Website':
+        //     return '#11585b';
+        // case 'Ecommerce Website':
+        //     return '#a31893';
+        // case 'Educational Website':
+        //     return '#1d7711';
+        // case 'Web Hosting Website':
+        //     return '#371177';
+        default:
+            return '#000'; 
+    }
+};
