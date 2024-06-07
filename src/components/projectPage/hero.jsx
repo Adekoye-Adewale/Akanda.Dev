ad7c24967989280751efd0ad0c470e5bf981ed30
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-06-07 03:31:27 +0100

    Create hero.jsx

---
+++ src/components/projectPage/hero.jsx
@@ -0,0 +1,57 @@
+'use client'
+import {useRef} from 'react';
+import { useScroll, useTransform, motion } from "framer-motion";
+import Image from 'next/image';
+import { Heading } from '../text';
+import style from './projectPage.module.css';
+
+export default function Hero({ img, title, status, year, agency }) {
+
+    const container = useRef();
+    const { scrollYProgress } = useScroll({
+        target: container,
+        offset: ["start start", "end start"]
+    })
+
+    const scale = useTransform(scrollYProgress, [0, 1], [1, 2]);
+    const clipPath = useTransform(scrollYProgress, [0, 1], ["circle(35% at 50% 50%)", "circle(80% at 50% 50%)"]);
+    const translateY = useTransform(scrollYProgress, [0, 1], [0, -200]);
+
+    return (
+        <section ref={container} className={`${style.hero} inline__pad`}>
+            <motion.div 
+                style={{scale, clipPath}} 
+                transition={{ 
+                    type: "spring", 
+                    stiffness: 100, 
+                    ease: "easeInOut"
+                }}
+                className={style.hero__img__wrap}
+            >
+                <Image {...img}/>
+            </motion.div>
+            <div 
+                className={style.hero__copy__wrap}
+            >
+                <motion.div 
+                    style={{translateY}} 
+                    transition={{ 
+                        type: "spring", 
+                        stiffness: 100, 
+                        ease: "easeInOut"
+                    }}
+                >
+                    <Heading 
+                        head={title}
+                    />
+                </motion.div>
+                
+                <div className={style.hero__metadata__wrap}>
+                    <span>Project Status: {status}</span>
+                    <span>Since {year}</span>
+                    <span>Agency: {agency}</span>
+                </div>
+            </div>
+        </section>
+    )
+}
