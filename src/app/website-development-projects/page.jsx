8cc5ef6d7dd9e695e3928a35fc8cab39d34f7599
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-06-07 03:30:53 +0100

    Update page.jsx

--- src/app/website-development-projects/page.jsx
+++ src/app/website-development-projects/page.jsx
@@ -2,8 +2,6 @@ import PortfolioContents from "@/components/pages/PortfolioPageComponents";
 
 export default async function ProjectPage() {
 
-
-    // console.log('pOrt::', workContent);
     return (
         <>
             <PortfolioContents/>
