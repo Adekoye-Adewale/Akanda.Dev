d17fcc996ae60bf06ab13c5e49190060ece79a8b
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-06-11 13:23:37 +0100

    Update singleProjectPage.jsx

--- src/components/pages/PortfolioPageComponents/singleProjectPage.jsx
+++ src/components/pages/PortfolioPageComponents/singleProjectPage.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 import Hero from '@/components/projectPage/hero'
 import CtaWrap from '@/components/siteFooter/ctaWrap';
 import ServicesProvided from '@/components/projectPage/servicesProvides';
+import MainContent from '@/components/projectPage/mainContent';
 
 export default function SingleProjectPage({ params }) {
 
-    const { img, title, status, projectYear, agency }  = params;
+    const { img, title, status, projectYear, agency, serviceProvided, objectiveSummary, mainService, mainDescription, techList, imgList }  = params;
+    const mainWorksCopy = {
+        title: title,
+        serviceProvided: serviceProvided,
+        objectiveSummary: objectiveSummary,
+        mainService: mainService,
+        mainDescription: mainDescription,
+        techList: techList,
+    }
 
     return (
         <main>
@@ -15,6 +24,10 @@ export default function SingleProjectPage({ params }) {
                 status={status} 
                 year={projectYear} 
                 agency={agency}
+            />
+            <MainContent 
+                mainWorksCopy={mainWorksCopy} 
+                // imgList={imgList}
             />
             <ServicesProvided/>
             <CtaWrap/>
