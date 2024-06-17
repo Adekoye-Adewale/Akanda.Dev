ace57087322ad42c17ee22aa9b37dcecbb8a6a4a
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-06-18 00:00:58 +0100

    Create layout.jsx
    imported and used the schema in the layout for better optimization

---
+++ src/app/blog/[title]/layout.jsx
@@ -0,0 +1,20 @@
+import Script from "next/script";
+import { generateMetadata } from './page';
+
+export default async function Layout({ children, params }) {
+    const { schema } = await generateMetadata({params});
+    return (
+        <>
+            <Script
+                id="blog-schema"
+                type="application/ld+json"
+                dangerouslySetInnerHTML={{
+                    __html: JSON.stringify(schema)
+                }}
+            />
+            <main>
+                {children}
+            </main>
+        </>
+    );
+}
