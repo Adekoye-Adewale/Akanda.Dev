c6819f39bd4872a719fd04897587be727af9e6ea
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-06-20 21:34:19 +0100

    Update hero.jsx

--- src/components/aboutPage/hero.jsx
+++ src/components/aboutPage/hero.jsx
@@ -16,7 +16,7 @@ export default function Hero() {
 
     const scale = useTransform(scrollYProgress, [0, 1], [0.8, 500]);
     const backgroundColor = useTransform(scrollYProgress, [0, 1], ["#ffffff", '#000000']);
-    const opacity = useTransform(scrollYProgress, [0, 0.65, 1], [0, 0, 1]);
+    const opacity = useTransform(scrollYProgress, [0, 0.50, 0.8, 1], [0, 0, 1, 1]);
     const filter = useTransform(scrollYProgress, [0, 0.6], ["blur(0) brightness(1)", "blur(10px) brightness(0)"]);
     const transform = useTransform(scrollYProgress, [0, 0.35, 1], 
         ["scale(0.8) translate(0px, 0px)", "scale(1) translate(-15%, 0%)", "scale(50) translate(-15%, 50%)"]);
@@ -53,12 +53,6 @@ export default function Hero() {
                     ease: "easeInOut"
                 }}
             >
-                {/* <h1>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum 
-                </h1>
-                <p>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum 
-                </p> */}
                 <HeroSecTwo/>
             </motion.div>
         </section>
