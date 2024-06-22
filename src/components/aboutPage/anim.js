bc209e65d3f7bd8089a6c130207f70517eafe84a
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-06-22 22:29:55 +0100

    Update anim.js

--- src/components/aboutPage/anim.js
+++ src/components/aboutPage/anim.js
@@ -1,40 +1,82 @@
 export const tabCopy = {
-    initial: {
-        height: 0,
-        opacity: 0,
-        y: 10,
-    },
-    enter: {
-        height: 90,
-        opacity: 1,
-        y: 0,
-        transition: {
-            duration: 0.75,
-            delay: 0,
-            ease: "linear",
-            y: {
-                duration: 0.5,
-                delay: 0.5,
-            },
-            opacity: {
+    desktop: {
+        initial: {
+            height: 0,
+            opacity: 0,
+            y: 10,
+        },
+        enter: {
+            height: 90,
+            opacity: 1,
+            y: 0,
+            transition: {
                 duration: 0.75,
-                delay: 0.25,
+                delay: 0,
+                ease: "linear",
+                y: {
+                    duration: 0.75,
+                    delay: 0.25,
+                },
+                opacity: {
+                    duration: 1.5,
+                    delay: 0.35,
+                }
+            }
+        },
+        exit: {
+            height: 0,
+            opacity: 0,
+            y: -20,
+            transition: {
+                delay: 0.35,
+                duration: 0.5,
+                type: "tween",
+                ease: [0.17, 0.67, 0.83, 0.67],
+                opacity: {
+                    duration: 1,
+                    delay: 0.25,
+                }
             }
         }
     },
-    exit: {
-        height: 0,
-        opacity: 0,
-        y: -20,
-        transition: {
-            delay: 0.35,
-            duration: 0.5,
-            type: "tween",
-            ease: [0.17, 0.67, 0.83, 0.67],
-            opacity: {
-                duration: 1,
-                delay: 0.25,
+    mobile: {
+        initial: {
+            height: 0,
+            opacity: 0,
+            y: 10,
+        },
+        enter: {
+            height: 150,
+            opacity: 1,
+            y: 0,
+            transition: {
+                duration: 0.75,
+                delay: 0,
+                ease: "linear",
+                y: {
+                    duration: 0.75,
+                    delay: 0.25,
+                },
+                opacity: {
+                    duration: 1.5,
+                    delay: 0.35,
+                }
+            }
+        },
+        exit: {
+            height: 0,
+            opacity: 0,
+            y: -20,
+            transition: {
+                delay: 0.35,
+                duration: 0.5,
+                type: "tween",
+                ease: [0.17, 0.67, 0.83, 0.67],
+                opacity: {
+                    duration: 1,
+                    delay: 0.25,
+                }
             }
         }
-    }
-};
+    } 
+}
