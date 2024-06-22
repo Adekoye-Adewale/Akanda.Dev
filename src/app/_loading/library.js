f7103eed4638798075fcdba84f3674801b2a04b7
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-06-22 22:29:51 +0100

    Update library.js

--- src/app/_loading/library.js
+++ src/app/_loading/library.js
@@ -2,9 +2,9 @@ export const handleSeeMoreClick = (facts, setFacts) => {
     const randomIndex = Math.floor(Math.random() * facts.length);
     const updatedFacts = facts.map((fact, index) => {
         if (index === randomIndex) {
-            return { ...fact, opacity: 1 }; // Update opacity to 1 for the randomly selected fact
+            return { ...fact, opacity: 1 }; 
         }
-        return { ...fact, opacity: 0 }; // Reset opacity to 0 for other facts
+        return { ...fact, opacity: 0 };
     });
     setFacts(updatedFacts);
 };
