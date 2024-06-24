b85dd03ff10b945e28bb63641008fe995e9f7692
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-06-24 23:03:26 +0100

    Update introSec.jsx

--- src/components/aboutPage/introSec.jsx
+++ src/components/aboutPage/introSec.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
-import { Heading, SubTitle } from '../text';
+'use client'
+import React, { useRef } from 'react';
+import { useScroll, useTransform, motion } from "framer-motion";
+import { Heading, Title } from '../text';
 import Image from 'next/image';
 import { Akanda } from '@/webContents/aboutPage';
+import style from './about.module.css'
 
 export default function IntroSec() {
 
-    const intro = Akanda.alwaysReady
+    const container = useRef();
+    const { scrollYProgress } = useScroll({
+        target: container,
+        offset: ["start end", "end start"]
+    });
+    const y = useTransform(scrollYProgress, [0, 1], [-60, 0]);
+    
+    const intro = Akanda.alwaysReady;
 
     return (
-        <section>
-            <div>
+        <section 
+            
+            className={style.intro__sec}
+        >
+            <div className={style.intro__sec__title}>
                 <Heading head={intro.title}/>
             </div>
-            <div>
-                <Image {...intro.img}/>
+            <div ref={container} className={style.intro__sec__img}>
+                <motion.div 
+                    style={{
+                        y
+                    }} 
+                    className={style.intro__sec__img__wrap}
+                >
+                    <Image {...intro.img}/>
+                </motion.div>
             </div>
-            <div>
-                <SubTitle subTitle={intro.subTitle}/>
+            <div className={style.intro__sec__sub__title}>
+                <Title title={intro.subTitle}/>
             </div>
         </section>
     )
