50cbbbadb6967f97b9c5ef2df5519fee911b4087
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-06-24 21:51:02 +0100

    Update index.jsx

--- src/components/aboutPage/index.jsx
+++ src/components/aboutPage/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './hero'
+import IntroSec from './introSec'
 
 export default function AboutPageComponent() {
     return (
         <>
             <Hero/>
+            <IntroSec/>
         </>
     )
 }
