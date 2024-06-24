817440c04c7e6b2a117ecce9705be3ccbef8288b
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-06-24 23:03:30 +0100

    Update index.jsx

--- src/components/aboutPage/index.jsx
+++ src/components/aboutPage/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './hero'
 import IntroSec from './introSec'
+import CtaWrap from '../siteFooter/ctaWrap'
 
 export default function AboutPageComponent() {
     return (
         <>
             <Hero/>
             <IntroSec/>
+            <CtaWrap/>
         </>
     )
 }
