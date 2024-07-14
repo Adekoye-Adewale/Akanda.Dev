82d455bc852c7e297af0a9fee1cd0cacfb1c057d
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-07-14 21:36:40 +0100

    Update introSec.jsx

--- src/components/aboutPage/introSec.jsx
+++ src/components/aboutPage/introSec.jsx
@@ -1,25 +1,24 @@
 'use client'
 import React, { useRef } from 'react';
 import { useScroll, useTransform, motion } from "framer-motion";
-import { Heading, Title } from '../text';
+import { Heading, SubTitle } from '../text';
 import Image from 'next/image';
 import { Akanda } from '@/webContents/aboutPage';
-import style from './about.module.css'
+import style from './about.module.css';
 
 export default function IntroSec() {
 
     const container = useRef();
     const { scrollYProgress } = useScroll({
         target: container,
-        offset: ["start end", "end start"]
+        offset: ["30% end", "70% start"]
     });
-    const y = useTransform(scrollYProgress, [0, 1], [-60, 0]);
+    const y = useTransform(scrollYProgress, [0, 1], [ 0, -180]);
     
     const intro = Akanda.alwaysReady;
 
     return (
         <section 
-            
             className={style.intro__sec}
         >
             <div className={style.intro__sec__title}>
@@ -36,7 +35,7 @@ export default function IntroSec() {
                 </motion.div>
             </div>
             <div className={style.intro__sec__sub__title}>
-                <Title title={intro.subTitle}/>
+                <SubTitle subTitle={intro.subTitle}/>
             </div>
         </section>
     )
