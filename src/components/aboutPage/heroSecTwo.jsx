2be606143ac42175797c0358377eb3de5170bc2a
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-07-16 21:48:28 +0100

    refact: clean up code
    removed unwatched codes

--- src/components/aboutPage/heroSecTwo.jsx
+++ src/components/aboutPage/heroSecTwo.jsx
@@ -1,39 +1,12 @@
-'use client'
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Image from 'next/image';
-import { motion } from 'framer-motion';
 import { Body, SubTitle } from '../text';
-import { tabCopy } from './anim';
-import style from './about.module.css';
 import { Tabs } from '@/webContents/skills';
+import style from './about.module.css';
 
 export default function HeroSecTwo() {
 
     const whyAkandaList = Tabs;
-    // const [hoverTab, setHoverTab] = useState(whyAkandaList[0]);
-    // const [hoveredIndex, setHoveredIndex] = useState(null);
-    // const [isMobile, setIsMobile] = useState(false);
-
-    // useEffect(() => {
-    //     const checkMobile = () => {
-    //         setIsMobile(window.innerWidth <= 768);
-    //     };
-    //     checkMobile();
-    //     window.addEventListener('resize', checkMobile);
-    //     return () => window.removeEventListener('resize', checkMobile);
-    // }, []);
-
-    // const variant = isMobile ? tabCopy.mobile : tabCopy.desktop;
-
-    // const handleMouseEnter = (list, index) => {
-    //     setHoverTab(list);
-    //     setHoveredIndex(index);
-    // };
-
-    // const handleMouseLeave = () => {
-    //     setHoverTab(whyAkandaList[0]);
-    //     setHoveredIndex(null);
-    // };
 
     return (
         <div 
@@ -42,32 +15,27 @@ export default function HeroSecTwo() {
             <div 
                 className={style.list__wrap}
             >
-                {/* <div 
-                    className={style.list__img__wrap}
-                >
-                    {hoverTab && hoverTab.comp(
-                        { 
-                            aboutImg: hoverTab.aboutImg,
-                        })
-                    }
-                </div> */}
                 {whyAkandaList.map((list, i) => (
-                    <div key={i} style={{ '--r': i }} className={style.list__copy__wrap}>
-                        {/* <span></span> */}
+                    <div 
+                        key={i} 
+                        style={{ '--r': i }} 
+                        className={style.list__copy__wrap}
+                    >
                         <div>
                             <SubTitle 
                                 subTitle={list.label}
                             />
                         </div>
-                        <div className={style.list__img__wrap}>
+                        <div 
+                            className={style.list__img__wrap}
+                        >
                             <Image {...list.aboutImg}/>
                         </div>
                         <div>
                             <Body 
                                 text={list.text}
                             />
-                        </div>
-                        
+                        </div>   
                     </div>
                 ))}
             </div>
