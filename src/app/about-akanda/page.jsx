6a21c01dce0ee6074e3a39fcbd7830c97ef68e5f
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-07-16 21:49:09 +0100

    Added metadata description for about page

--- src/app/about-akanda/page.jsx
+++ src/app/about-akanda/page.jsx
@@ -2,6 +2,7 @@ import AboutPageComponent from "@/components/aboutPage";
 
 export const metadata = {
     title: "About Akanda Dev",
+    description: "Specializing in creating exceptional digital experiences, Adekoye Adewale (Akanda) is the top pick for all of your website design and development needs.",
     alternates: {
         canonical: `/about-akanda`,
     },
@@ -12,11 +13,6 @@ export default function AboutPage() {
     return (
         <main>
             <AboutPageComponent/>
-            {/* <div className='grid__center full__size'>
-                <h1>
-                    About
-                </h1>
-            </div> */}
         </main>
     )
 }
