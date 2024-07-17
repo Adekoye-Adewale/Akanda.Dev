6e4eb9174942f27ae00bf3a4837c432ea95eb63e
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-07-17 16:08:35 +0100

    Update index.jsx

--- src/components/sectionFooter/index.jsx
+++ src/components/sectionFooter/index.jsx
@@ -1,26 +1,73 @@
-import React from 'react'
+'use client'
+import React, { useRef } from 'react'
+import { circInOut, motion, useScroll, useTransform } from 'framer-motion'
 import Link from 'next/link'
 import { Body } from '../text'
 import style from './sectionFoot.module.css'
 
 export default function SectionFoot() {
+    
+    const container = useRef();
+
+    const { scrollYProgress } = useScroll({
+        target: container,
+        offset: ["end 99.5%", "end 90%"]
+    });
+
+    const y = useTransform(scrollYProgress, [0, 1], [ 0, -180], { ease: circInOut });
+    const opacity = useTransform(scrollYProgress, [0, 0.6], [ 1, 0], { ease: circInOut });
+    const maxHeight = useTransform(scrollYProgress, [0, 1], [ 80, 0]);
+    const paddingBottom = useTransform(scrollYProgress, [0, 1], [ 20, 0]);
+    const paddingTop = useTransform(scrollYProgress, [0, 1], [ 20, 0]);
+
+    console.log('=============', opacity);
+    console.log('+++++++++++++', scrollYProgress);
+
     return (
-        <div className={style.wrap}>
-            <Link 
-                href={'https://mobile.twitter.com/__akanda__'} 
-                target={'_blank'}
-                title={'Twitter Link'}
+        <motion.div 
+            className={`${style.wrap} sec__foot`}
+            ref={container}
+            style={{
+                maxHeight, paddingTop, paddingBottom
+            }}
+        >
+            <motion.div
+                style={{
+                    opacity,
+                    y
+                }}
             >
-                @__akanda__
-            </Link>
-            <Body text={"A Creative & Problem Solving Software Engineer From Nigeria"}/>
-            <Link 
-                href={''} 
-                target={'_blank'} 
-                title={'GitHub Link'}
+                <Link 
+                    href={'https://mobile.twitter.com/__akanda__'} 
+                    target={'_blank'}
+                    title={'Twitter Link'}
+                >
+                    @__akanda__
+                </Link>
+            </motion.div>
+            <motion.div
+                style={{
+                    opacity,
+                    y
+                }}
             >
-                A Creator of Things On The WorldWideWeb
-            </Link>
-        </div>
+                <Body 
+                    text={"A Creative & Problem Solving Software Engineer From Nigeria"}
+                />
+            </motion.div>
+            <motion.div
+                style={{
+                    opacity, y
+                }}
+            >
+                <Link 
+                    href={''} 
+                    target={'_blank'} 
+                    title={'GitHub Link'}
+                >
+                    A Creator of Things On The WorldWideWeb
+                </Link>
+            </motion.div>            
+        </motion.div>
     )
 }
