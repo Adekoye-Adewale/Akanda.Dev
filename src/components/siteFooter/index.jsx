b49aed0aeda1032892a4d65db9340de7690b6356
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-07-18 23:02:44 +0100

    Update index.jsx

--- src/components/siteFooter/index.jsx
+++ src/components/siteFooter/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-// import CtaWrap from './ctaWrap'
 import Foot from './foot'
 
 export default function SiteFooter() {
     return (
         <footer>
-            {/* <CtaWrap/> */}
             <Foot/>
         </footer>
     )
