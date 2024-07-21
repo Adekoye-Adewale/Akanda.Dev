0b01c6ac63f568037efc11f5b1d3b28bea37f805
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-07-21 20:53:49 +0100

    Create form.js

---
+++ src/webContents/form.js
@@ -0,0 +1,87 @@
+export const projectTypeOptions = [
+    {
+        value: `portfolio`,
+        label: `Portfolio Website`,
+    },
+    {
+        value: `blog`,
+        label: `Blog Site`,
+    },
+    {
+        value: `corporate`,
+        label: `Corporate Website`,
+    },
+    {
+        value: `ecommerce`,
+        label: `eCommerce`,
+    },
+    {
+        value: `realEstate`,
+        label: `Real Estate`,
+    },
+    {
+        value: `banking`,
+        label: `Banking`,
+    },
+    {
+        value: `other`,
+        label: `Other`,
+    },
+];
+
+export const budgetOptions = [
+    {
+        value: `99To999`,
+        label: `$99 to $999`,
+    },
+    {
+        value: `1000To4999`,
+        label: `$1,000 to $4,999`,
+    },
+    {
+        value: `5000To9999`,
+        label: `$5,000 to $9,999`,
+    },
+    {
+        value: `moreThan10000`,
+        label: `more Than $10,000`,
+    },
+];
+
+export const businessOnlineOptions = [
+    {
+        value: `website`,
+        label: `Yes, I have a Website`,
+    },
+    {
+        value: `socialMedia`,
+        label: `Yes, using a social media account`,
+    },
+    {
+        value: `listingSite`,
+        label: `Yes, using a listing site`,
+    },
+    {
+        value: `no`,
+        label: `No`,
+    },
+];
+
+export const startTimingOptions = [
+    {
+        value: `rightNow`,
+        label: `As soon as right now`,
+    },
+    {
+        value: `1-2weeks`,
+        label: `In a week or 2 weeks time`,
+    },
+    {
+        value: `fewMonths`,
+        label: `In few months time`,
+    },
+    {
+        value: `notSure`,
+        label: `Not so sure for now`,
+    },
+];
