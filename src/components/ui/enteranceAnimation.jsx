a894ff00091a664e8ec22f5b6421649f4afe61d7
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-07-27 13:40:16 +0100

    Update enteranceAnimation.jsx

--- src/components/ui/enteranceAnimation.jsx
+++ src/components/ui/enteranceAnimation.jsx
@@ -1,21 +1,23 @@
 import { useRef } from "react";
 import { useInView } from "framer-motion";
 
-export function FadeInLeft({ children }) {
+export function FadeInLeft({ children, delay }) {
   const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
+  const isInView = useInView(ref, 
+                                // { once: true }
+                            );
 
   return (
-    <div ref={ref}>
-        <span
-            style={{
-                transform: isInView ? "none" : "translateX(-200px)",
-                opacity: isInView ? 1 : 0,
-                transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
-            }}
-        >
+    <div 
+        ref={ref} 
+        style={{
+            transform: isInView ? "translateX(0px)" : "translateX(-200px)",
+            opacity: isInView ? 1 : 0,
+            transition: `all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1)`,
+            transitionDelay: delay
+        }}
+    >
             {children}
-        </span>
     </div>
   );
 }
