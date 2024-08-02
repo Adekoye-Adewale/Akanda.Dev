907f6109527a0eef4d1df72cbbf99178574ab51b
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-08-02 23:43:40 +0100

    Update anim.js

--- src/components/header/Nav/anim.js
+++ src/components/header/Nav/anim.js
@@ -40,11 +40,11 @@ export const slideIn = {
         y: 0,
         transition: { 
             duration: 0.5,
-            delay: 0.75 + (i * 0.1), 
+            delay: 0.85 + (i * 0.1), 
             ease: [.215,.61,.355,1],
             opacity: {
                 duration: 1,
-                delay: 1.35 + (i * 0.1)
+                delay: 0.85 + (i * 0.1)
             }
         }
     }),
