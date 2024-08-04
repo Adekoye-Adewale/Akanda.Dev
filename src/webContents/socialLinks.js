78752518cc3a1ed32117c128f4e4c9c9c075042c
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-08-04 21:04:44 +0100

    Update socialLinks.js

--- src/webContents/socialLinks.js
+++ src/webContents/socialLinks.js
@@ -6,24 +6,36 @@ export const fbLink = {
 
 export const xLink = {
     href: `https://twitter.com/__akanda__`,
-    title: `facebook link`,
+    title: `twitter link`,
     target: `_blank`,
 }
 
 export const linkedinLink = {
     href: `https://www.linkedin.com/in/adekoye-adewale/`,
-    title: `facebook link`,
+    title: `LinkedIn link`,
     target: `_blank`,
 }
 
 export const mailLink = {
     href: `https://twitter.com/__akanda__`,
-    title: `facebook link`,
+    title: `eMail link`,
     target: `_blank`,
 }
 
 export const mediumLink = {
     href: `https://twitter.com/__akanda__`,
-    title: `facebook link`,
+    title: `medium link`,
+    target: `_blank`,
+}
+
+export const gitHubLink = {
+    href: `https://github.com/Adekoye-Adewale`,
+    title: `github link`,
+    target: `_blank`,
+}
+
+export const telegramLink = {
+    href: `https://t.me/adisaempire`,
+    title: `telegram`,
     target: `_blank`,
 }
