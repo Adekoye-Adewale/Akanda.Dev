f666348fef548a9d1b2f08ddca3fb6a12e539d73
AdisaEmpire <94379177+Adekoye-Adewale@users.noreply.github.com>
2024-08-04 21:03:14 +0100

    Update socialMedia.jsx

--- src/components/contactInfo/socialMedia.jsx
+++ src/components/contactInfo/socialMedia.jsx
@@ -3,11 +3,11 @@ import { Title } from '../text'
 import style from './contactInfo.module.css'
 import { SocialIcon } from '../socials'
 
-export default function SocialMedia({ title, Socials }) {
+export default function SocialMedia({ title }) {
     return (
         <div className={style.social}>
             <Title title={title}/>
-            <div>
+            <div className={style.socials__wrap}>
                 <SocialIcon/>
             </div>
         </div>
